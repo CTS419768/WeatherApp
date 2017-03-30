6f24893712990d9f7a94f96cb6f5691bb82fb94b
Abhir Vaidya <vaidyabhir@gmail.com>
2017-03-30 13:05:39 +0530

    Source Added

---
+++ myWeather/WeatherContentDetailPageViewController.swift
@@ -0,0 +1,55 @@
+//
+//  WeatherContentDetailPageViewController.swift
+//  myWeather
+//
+//  Created by Abhir Vaidya on 29/03/17.
+//  Copyright © 2017 Abhir Vaidya. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WeatherContentDetailPageViewController: UIViewController {
+    @IBOutlet weak var cityName: UILabel!
+    
+    @IBOutlet weak var visibilityValue: UILabel!
+    @IBOutlet weak var humadityValue: UILabel!
+
+    @IBOutlet weak var temperature: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var weatherCondition: UILabel!
+
+    @IBOutlet weak var windSpeedValue: UILabel!
+    
+    var pageIndex: Int = 0
+    var currentCity : String?
+    var currentCondition : CurrentCondition?
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+//        imageView.image = UIImage(named: strPhotoName)
+//        lblTitle.text = strTitle
+        
+     
+    }
+ 
+    func updateUI(){
+        guard let current = currentCondition else {
+            return
+        }
+        visibilityValue.text = current.visibility
+        humadityValue.text = current.humidity
+        windSpeedValue.text = current.windSpeedMiles
+        weatherCondition.text = current.weatherDesc
+        temperature.text = current.temperature
+        date.text = current.observationTime
+        weatherImage.imageFromServerURL(urlString: current.weatherIconUrl!)
+        cityName.text = currentCity
+        
+        
+        
+    }
+    
+}
